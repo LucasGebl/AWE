ef04abf33eec1c926da869f8fe40629a5f4cde29
Fabian Steiner <fabi.steiner@gmx.net>
2021-06-14 19:19:00 +0200

    Hello World!

--- betriebsmittelverwaltung/Startup.cs
+++ betriebsmittelverwaltung/Startup.cs
@@ -34,7 +34,7 @@ namespace betriebsmittelverwaltung
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-
+        //Hallo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
