ebefdf0a92f0a03f5e86132e1dcd69b6a0ad9ca5
Justin Röttinger <justin@rodomnia.com>
2021-06-15 18:47:37 +0200

    Identity init

---
+++ betriebsmittelverwaltung/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using betriebsmittelverwaltung.Areas.Identity.Data;
+using betriebsmittelverwaltung.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(betriebsmittelverwaltung.Areas.Identity.IdentityHostingStartup))]
+namespace betriebsmittelverwaltung.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AppDBContextConnection")));
+
+                services.AddDefaultIdentity<User>()
+                    .AddEntityFrameworkStores<AppDBContext>();
+            });
+        }
+    }
+}
